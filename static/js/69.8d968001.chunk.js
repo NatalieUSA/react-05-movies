c7319206c46a7b4f12652e58749e5be22d2f902c
NatalieUSA <110269066+NatalieUSA@users.noreply.github.com>
2023-03-15 06:39:50 +0000

    Deploying to gh-pages from @ NatalieUSA/goit-react-hw-05-movies@3991c160125c6e8320e829f12885dbe3bc263117 🚀

---
+++ static/js/69.8d968001.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[69],{69:function(e,r,t){t.r(r);var s=t(184);r.default=function(){return(0,s.jsxs)("div",{children:[(0,s.jsx)("h2",{children:"Movies"}),(0,s.jsx)("p",{children:"MoviesPage"})]})}}}]);
+//# sourceMappingURL=69.8d968001.chunk.js.map
---
+++ static/js/890.dcc848a0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[890],{890:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:(0,a.jsx)("h4",{children:"MovieDetailsPage"})})}}}]);
+//# sourceMappingURL=890.dcc848a0.chunk.js.map
